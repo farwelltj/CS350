cee27ed9dbe0d2ec6954d92c0bdb6fd065276a7b
farwelltj <farwelltj1988@gmail.com>
2015-04-14 21:41:33 +0000

    page 2

--- hello.py
+++ hello.py
@@ -4,5 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return '<h1>Hello World!'</h1>
+ 
+@app.route('/user/<name>')
+def user(name):
+	return '<h1> Hello, %s!</h1>' % name
+
+if __name__ == '__name__':
+	app.run(debug=True)
