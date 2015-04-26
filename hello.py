48c947b077c35a2b8be3999ed1ede55eb1d6e180
farwelltj <farwelltj1988@gmail.com>
2015-04-26 02:18:04 +0000

    add route

--- hello.py
+++ hello.py
@@ -1,15 +1,18 @@
-import os
-from flask import Flask
-
+from flask import Flask, render_template
+  
 app = Flask(__name__)
-
+  
 @app.route('/')
-def index():
-    return '<h1>Hello World!'</h1>
- 
-@app.route('/user/<name>')
-def user(name):
-	return '<h1> Hello, %s!</h1>' % name
+def home():
+  return render_template('home.html')
+  
+@app.route('/about')
+def about():
+  return render_template('about.html')
 
-if __name__ == '__name__':
-	app.run(debug=True)
+@app.route('/about')
+def about():
+  return render_template('login.htmml')
+  
+if __name__ == '__main__':
+  app.run(debug=True)
